f18ebe7495e152a9ed630a24dde5472a4c3c024b
Andaluzov <zhurbasi@gmail.com>
2020-06-05 23:16:24 +0300

    add matrix

---
+++ goal.py
@@ -0,0 +1,28 @@
+# noinspection PyUnresolvedReferences
+import matrtools as mt
+A = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+]
+B = [
+  [0, 2],
+  [1, 0],
+  [4, 7]
+]
+#print('A=',A)
+#print('B=',B)
+
+mt.add(A, B)
+
+# Тест2
+
+A = [
+  [0, 2, 9],
+  [-5, 1, 1]
+]
+B = [
+  [0, 2, 0],
+  [1, 0, 3]
+]
+mt.add(A, B)
---
+++ matrtools.py
@@ -0,0 +1,11 @@
+def add(A,B):
+    Crow=[]
+
+    for i in range(len(A)):
+        ccol=[]
+        for j in range(len(A[0])):
+            ccol.append(A[i][j]+B[i][j])
+
+        Crow.append(ccol)
+    print(Crow)
+    return Crow
