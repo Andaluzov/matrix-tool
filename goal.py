23582807c1c106b4af1eca1c3152b0d13beac080
Andaluzov <zhurbasi@gmail.com>
2020-06-30 22:14:32 +0300

    Transponirovanie

--- goal.py
+++ goal.py
@@ -1,10 +1,47 @@
 # noinspection PyUnresolvedReferences
 import matrtools as mt
+print('сложение матриц')
+matr_a = [[1, 2],[3, 4],[5, 6]]
+matr_b = [[0, 2],[1, 0],[4, 7]]
 
-matr_a=[[1, 2],[3, 4],[5, 6]]
-matr_b=[[0, 2],[1, 0],[4, 7]]
-
-
-rez_matr=mt.add(matr_a, matr_b)
+rez_matr = mt.add(matr_a, matr_b)
 mt.print_matrix(rez_matr)
 
+#2
+print('умножение матриц')
+matr_a  = [ [1, 2, 1], [3, 4, 5]]
+matr_b = [ [0, 2], [1, 0], [1, 5]]
+
+rez_matr = mt.multiply(matr_a, matr_b)
+print('multiply of matrix AxB')
+mt.print_matrix(rez_matr)
+rez_matr = mt.multiply(matr_b, matr_a)
+print('multiply of matrix BxA')
+mt.print_matrix(rez_matr)
+
+#3
+print('Транспонирование матриц')
+matr_a = [
+ [1, 0],
+ [4, 1]
+]
+print('1')
+a_transp = mt.transpose(matr_a)
+mt.print_matrix(a_transp)
+matr_a = [
+ [1, 2],
+ [3, 4],
+ [5, 6]
+]
+print('2')
+a_transp = mt.transpose(matr_a)
+mt.print_matrix(a_transp)
+
+matr_a = [
+ [1, 2],
+ [3, 4],
+ [5, 6]
+]
+print('3')
+matr_b= mt.transpose(mt.transpose(matr_a))
+mt.print_matrix(matr_b)
--- matrtools.py
+++ matrtools.py
@@ -13,3 +13,45 @@ def print_matrix(a):
     for i in range(len(a)):
        print(a[i])
 
+
+def multiply(a,b):
+    '''
+    # A
+    # m x p
+    #B
+    #p x n
+
+    '''
+
+    matr_c = []
+    m = len(a)
+    p1 = len(a[0])
+    p = len(b)
+    n = len(b[0])
+    if p != p1:
+        raise ValueError('Матрицы должны быть  размера mxp и pxn')
+    for i in range(m):
+        c = []
+        for j in range(n):
+            sum = 0
+            for k in range(p):
+                sum = sum+a[i][k]*b[k][j]
+            c.append(sum)
+        matr_c.append(c)
+
+    return matr_c
+
+def transpose(a):
+    ''''
+    Transponirovanie
+    '''
+    n = len(a)
+    m = len(a[0])
+    a_transp = []
+    for j in range(m):
+        c = []
+        for i in range(n):
+            c.append(a[i][j])
+        a_transp.append(c)
+
+    return(a_transp)
